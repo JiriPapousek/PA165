0b14291d675ac6a602c8b3ed2501f31001a3c28d
Jiří Papoušek <jiri.pap@gmail.com>
2021-03-11 15:34:30 +0100

    Add tests

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,39 +1,76 @@
 package cz.muni.fi.pa165.currency;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CurrencyConvertorImplTest {
 
+    private static final Currency CZK = Currency.getInstance("CZK");
+    private static final Currency EUR = Currency.getInstance("EUR");
+
+    @Mock
+    private ExchangeRateTable exchangeRateTable;
+
+    private CurrencyConvertor convertor = new CurrencyConvertorImpl(exchangeRateTable);
+
     @Test
-    public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+    public void testConvert() throws ExternalServiceFailureException {
+        MockitoAnnotations.initMocks(this);
+        when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenReturn(new BigDecimal("0.04"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("100"))).isEqualTo(new BigDecimal("4"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("1"))).isEqualTo(new BigDecimal("0.4"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("0"))).isEqualTo(new BigDecimal("0"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("100.149"))).isEqualTo(new BigDecimal("10.01"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("100.150"))).isEqualTo(new BigDecimal("10.02"));
     }
 
     @Test
     public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+        MockitoAnnotations.initMocks(this);
+        assertThatThrownBy(()->{convertor.convert(null, EUR, new BigDecimal("100"));})
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
+        MockitoAnnotations.initMocks(this);
+        assertThatThrownBy(()->{convertor.convert(EUR, null, new BigDecimal("100"));})
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void testConvertWithNullSourceAmount() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithNullSourceAmount() throws ExternalServiceFailureException {
+        MockitoAnnotations.initMocks(this);
+        when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenReturn(new BigDecimal("0.04"));
+        assertThatThrownBy(()->{convertor.convert(CZK, EUR, null);})
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void testConvertWithUnknownCurrency() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithUnknownCurrency() throws ExternalServiceFailureException {
+        MockitoAnnotations.initMocks(this);
+        when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenReturn(null);
+        assertThatThrownBy(()->{convertor.convert(CZK, EUR, new BigDecimal("100"));})
+                .isInstanceOf(UnknownExchangeRateException.class);
     }
 
     @Test
-    public void testConvertWithExternalServiceFailure() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
+        MockitoAnnotations.initMocks(this);
+        when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenThrow(ExternalServiceFailureException.class);
+        assertThatThrownBy(()->{convertor.convert(CZK, EUR, new BigDecimal("100"));})
+                .isInstanceOf(UnknownExchangeRateException.class);
     }
 
 }
