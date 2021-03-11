4b65288d66b58990022955f5cf9fd47b9a30072b
Jiří Papoušek <jiri.pap@gmail.com>
2021-03-11 16:35:16 +0100

    Fix tests.

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,8 +1,11 @@
 package cz.muni.fi.pa165.currency;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 import java.util.Currency;
@@ -14,6 +17,7 @@ import static org.mockito.Mockito.when;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
 
     private static final Currency CZK = Currency.getInstance("CZK");
@@ -22,44 +26,43 @@ public class CurrencyConvertorImplTest {
     @Mock
     private ExchangeRateTable exchangeRateTable;
 
-    private CurrencyConvertor convertor = new CurrencyConvertorImpl(exchangeRateTable);
+    private CurrencyConvertor convertor;
+
+    @Before
+    public void init() {
+        convertor = new CurrencyConvertorImpl(exchangeRateTable);
+    }
 
     @Test
     public void testConvert() throws ExternalServiceFailureException {
-        MockitoAnnotations.initMocks(this);
         when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenReturn(new BigDecimal("0.04"));
-        assertThat(convertor.convert(CZK, EUR, new BigDecimal("100"))).isEqualTo(new BigDecimal("4"));
-        assertThat(convertor.convert(CZK, EUR, new BigDecimal("1"))).isEqualTo(new BigDecimal("0.4"));
-        assertThat(convertor.convert(CZK, EUR, new BigDecimal("0"))).isEqualTo(new BigDecimal("0"));
-        assertThat(convertor.convert(CZK, EUR, new BigDecimal("100.149"))).isEqualTo(new BigDecimal("10.01"));
-        assertThat(convertor.convert(CZK, EUR, new BigDecimal("100.150"))).isEqualTo(new BigDecimal("10.02"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("100"))).isEqualTo(new BigDecimal("4.00"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("1"))).isEqualTo(new BigDecimal("0.04"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("0"))).isEqualTo(new BigDecimal("0.00"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("250.3725"))).isEqualTo(new BigDecimal("10.01"));
+        assertThat(convertor.convert(CZK, EUR, new BigDecimal("250.375"))).isEqualTo(new BigDecimal("10.02"));
     }
 
     @Test
     public void testConvertWithNullSourceCurrency() {
-        MockitoAnnotations.initMocks(this);
         assertThatThrownBy(()->{convertor.convert(null, EUR, new BigDecimal("100"));})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void testConvertWithNullTargetCurrency() {
-        MockitoAnnotations.initMocks(this);
         assertThatThrownBy(()->{convertor.convert(EUR, null, new BigDecimal("100"));})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void testConvertWithNullSourceAmount() throws ExternalServiceFailureException {
-        MockitoAnnotations.initMocks(this);
-        when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenReturn(new BigDecimal("0.04"));
         assertThatThrownBy(()->{convertor.convert(CZK, EUR, null);})
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void testConvertWithUnknownCurrency() throws ExternalServiceFailureException {
-        MockitoAnnotations.initMocks(this);
         when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenReturn(null);
         assertThatThrownBy(()->{convertor.convert(CZK, EUR, new BigDecimal("100"));})
                 .isInstanceOf(UnknownExchangeRateException.class);
@@ -67,7 +70,6 @@ public class CurrencyConvertorImplTest {
 
     @Test
     public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
-        MockitoAnnotations.initMocks(this);
         when(exchangeRateTable.getExchangeRate(CZK, EUR)).thenThrow(ExternalServiceFailureException.class);
         assertThatThrownBy(()->{convertor.convert(CZK, EUR, new BigDecimal("100"));})
                 .isInstanceOf(UnknownExchangeRateException.class);
