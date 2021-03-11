413ee94dfe720ba79471f91b91f08be54ce49b78
Jiří Papoušek <jiri.pap@gmail.com>
2021-03-11 16:36:20 +0100

    Implement CurrencyConvertorImpl.convert method.

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,6 +1,8 @@
 package cz.muni.fi.pa165.currency;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Currency;
 
 
@@ -20,7 +22,23 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (sourceCurrency == null) throw new IllegalArgumentException("sourceCurrency argument is empty.");
+        if (targetCurrency == null) throw new IllegalArgumentException("targetCurrency argument is empty.");
+        if (sourceAmount == null) throw new IllegalArgumentException("sourceAmount argument is empty.");
+
+        BigDecimal rate;
+        try {
+            rate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+        } catch (ExternalServiceFailureException e) {
+            throw new UnknownExchangeRateException("Lookup for current exchange rate failed.", e);
+        }
+
+        if (rate == null) throw new UnknownExchangeRateException(
+                "The exchange rate for specified pair of currencies is unknown.");
+
+        BigDecimal result = sourceAmount.multiply(rate);
+        result = result.setScale(2, RoundingMode.HALF_UP);
+        return result;
     }
 
 }
