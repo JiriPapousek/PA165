cd47920f6cbc7e69355d316888ec8caad64ea381
Jiří Papoušek <jiri.pap@gmail.com>
2021-03-18 14:50:26 +0100

    Change LoggingAspect to use slf4j instead of System class.

--- currency-convertor/src/main/java/cz/muni/fi/pa165/aspects/LoggingAspect.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/aspects/LoggingAspect.java
@@ -3,6 +3,8 @@ package cz.muni.fi.pa165.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
 
 import javax.inject.Named;
@@ -15,13 +17,18 @@ public class LoggingAspect {
 
     @Around("execution(public * *(..))")
     public Object logDuration(ProceedingJoinPoint pjp) throws Throwable {
+        Logger logger = LoggerFactory.getLogger(pjp.getSignature().getDeclaringType());
+
         StopWatch stopWatch = new StopWatch();
-        System.err.println("Calling method " + pjp.getSignature().getName() + " of " + pjp.getTarget().getClass()
-                + " with parameters " + Arrays.asList(pjp.getArgs()));
+        logger.info("Calling method {} with parameters {}.",
+                pjp.getSignature().getName(),
+                Arrays.asList(pjp.getArgs()));
+
         stopWatch.start();
         Object result = pjp.proceed();
         stopWatch.stop();
-        System.err.println("Method call finished, took " + stopWatch.getTotalTimeMillis() + " ms.");
+
+        logger.info("Method call finished, took {} ms.", stopWatch.getTotalTimeMillis());
         return result;
     }
 }
